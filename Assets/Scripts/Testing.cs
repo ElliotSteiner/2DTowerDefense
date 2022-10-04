aca1c978560bfb55d82314d08ca53f1b069abc26
Elliot Steiner <esteiner6469@student.pps.net>
2022-10-04 09:57:52 -0700

    Added grid for start of tower placement

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -4,9 +4,60 @@ using UnityEngine;
 
 public class Testing : MonoBehaviour
 {
-   
+    private Grid grid;
+
     private void Start()
     {
-        Grid grid = new Grid(4, 2, 10f);
+        grid = new Grid(18, 10, 1f, new Vector3(-9, -5));
+        //new Grid(4, 3, 5f, new Vector3(0, -20));
+        //new Grid(10, 10, 20f, new Vector3(-100f, -20));
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(GetMouseWorldPosition()));
+        }
+    }
+
+
+
+
+
+
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ()
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+    }
+
+    public static Vector3 GetMouseWorldPositionWithZ(Camera worldCamera)
+    {
+        return GetMouseWorldPositionWithZ(Input.mousePosition, worldCamera);
+    }
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+    
+    public static Vector3 GetDirToMouse (Vector3 fromPosition)
+    {
+        Vector3 mouseWorldPosition = GetMouseWorldPosition();
+        return (mouseWorldPosition - fromPosition).normalized;
     }
 }
