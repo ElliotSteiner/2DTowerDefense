73c28a37b14e62e66d8de9959a040948a1cf42a7
Banyan <BTree268@gmail.com>
2022-10-10 09:59:52 -0700

    Working on wave system

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+
+    private enum SpawnState { SPAWNING, FIGHTING, COUNTING };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int count;
+        public float rate;
+    }
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountdown;
+
+    private float searchCountdown = 1f;
+
+    private SpawnState state = SpawnState.COUNTING;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update()
+    {
+        if (state == SpawnState.FIGHTING)
+        {
+            if (!EnemyIsAlive())
+            {
+                WaveCompleted();
+            }
+            else
+            {
+                return;
+            }
+        }
+            if (waveCountdown <= 0)
+            {
+                if (state != SpawnState.SPAWNING)
+                {
+                    StartCoroutine(SpawnWave(waves[nextWave]));
+                }
+                else
+                {
+                    waveCountdown -= Time.deltaTime;
+                }
+            }
+        }
+
+    void WaveCompleted() 
+    {
+        Debug.Log("Wave Completed");
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1) 
+        {
+            nextWave = 0;
+            Debug.Log("Waves Looping..."); //this is temporary
+        }
+        nextWave++;
+    }
+
+        bool EnemyIsAlive()
+        {
+            searchCountdown -= Time.deltaTime;
+            if (searchCountdown <= 0f)
+            {
+                searchCountdown = 1f;
+                if (GameObject.FindGameObjectWithTag("MinorEnemy") == null)
+                { //You might have to make an if statement for each enemy tag
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        IEnumerator SpawnWave(Wave _wave)
+        {
+            Debug.Log("Spawning Wave: " + _wave.name);
+            state = SpawnState.SPAWNING;
+            for (int i = 0; i < _wave.count; i++)
+            {
+                SpawnEnemy(_wave.enemy);
+                yield return new WaitForSeconds(1f / _wave.rate);
+            }
+            state = SpawnState.FIGHTING;
+            yield break;
+        }
+
+        void SpawnEnemy(Transform _enemy)
+        {
+            Debug.Log("Spawning Enemy: " + _enemy.name);
+            Instantiate(_enemy, transform.position, transform.rotation);
+        }
+
+}
